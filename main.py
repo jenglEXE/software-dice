58f8e3236c7de69a625b147bfc30ea0e58a7f1a1
jengl <154405260+jenglEXE@users.noreply.github.com>
2026-09-08 21:13:37 -0700

    Rewrite main.py; finalize

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
 from pynput import keyboard
 from roll_the_dice import load_dice, roll_all
 from animation import animate_roll
+from gui import run
 
-def on_press(key):
-    if key == keyboard.Key.space:
-        animate_roll()
-        die1_choice, die2_choice, die3_choice = roll_all(dice)
-        print(f"""
- Rolls:
---------------- 
- Interface/Runtime paradigm: {die1_choice}
----------------
- Object/Category: {die2_choice}
----------------
- Modifier (friendly/creative flavor): {die3_choice}""")
-        print("Press space to roll again.")
-        print("Press esc to exit.")
-    elif key == keyboard.Key.esc:
-        return False
 
 if __name__ == "__main__":
-    dice = load_dice("dice.txt")
-    print("Press space to roll.")
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()  
+    run() 
